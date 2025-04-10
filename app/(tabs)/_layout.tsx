65bc669c5ed503eea2e2d2800d87b0a73f9fea09
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-04-10 17:23:26 +0200

    made some changes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,44 @@
 import { Tabs } from "expo-router";
+import {Ionicons} from "@expo/vector-icons";
+
 
 export default function TabLayout() {
   return(
     <Tabs>
       <Tabs.Screen 
-      name="index" 
+      name="Index" 
       options={{
-        headerTitle: "Home Page"
+        headerTitle: "Foods",
+        tabBarIcon: ({color}) => <Ionicons name="fast-food" size={24} color={color} />,
+        tabBarLabel: "Food",
+        tabBarActiveTintColor: "blue",
+        tabBarInactiveTintColor: "gray",
+        tabBarStyle: {
+          backgroundColor: "white",
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+        },
       }}
       />
+
+      <Tabs.Screen
+      name="Order"
+      options={{
+        headerTitle: "Order Page",
+        tabBarIcon: ({color}) => <Ionicons name="cart" size={24} color={color} />,
+        tabBarLabel: "Order",
+        tabBarActiveTintColor: "blue",
+        tabBarInactiveTintColor: "gray",
+        tabBarStyle: {
+          backgroundColor: "white",
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+        },
+      }}
+      />
+
     </Tabs>
   )
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>This will show the Meals</Text>
     </View>
   );
 }
