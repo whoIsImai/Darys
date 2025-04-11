8db025ff50fe46f49f6b9963a73825b9d779e298
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-04-11 10:27:34 +0200

    imported menu box

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
-import { Text, View, StyleSheet } from "react-native";
-
+import { Text, View, StyleSheet, ScrollView } from "react-native";
+import MenuBox
+ from "@/components/MenuBox";
 export default function index() {
   const styles = StyleSheet.create({
     container: {
@@ -7,14 +8,36 @@ export default function index() {
       justifyContent: "center",
       alignItems: "center",
     },
-    text: {
-      fontSize: 20,
-    },
+    header: {
+      fontSize: 24,
+      fontWeight: "bold",
+      marginBottom: 20,
+      textAlign: "center",
+      color: "#333",
+    }
   });
 
   return (
-    <View style={styles.container}>
-      <Text>This will show the meals.</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Text style={styles.header}>Welcome to Darys</Text>
+      <MenuBox
+        title="Chips"
+        onPress={() => {
+          console.log("Meals");
+        }}
+      />
+      <MenuBox
+        title="Meals"
+        onPress={() => {
+          console.log("Orders");
+        }}
+      />
+      <MenuBox
+        title="Singles"
+        onPress={() => {
+          console.log("Profile");
+        }}
+      />
+    </ScrollView>
   );
 }
