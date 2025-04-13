fb29aa6320a46b23418d6995a9b9d8884563e5ac
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-04-13 17:29:58 +0200

    made imports into meal menu

---
+++ components/MealMenu.tsx
@@ -0,0 +1,4 @@
+import MealMenu from '../assets/Meals.json'
+import { View, Text, StyleSheet, Pressable, SafeAreaView, FlatList} from 'react-native'
+import {Ionicons} from '@expo/vector-icons'
+import {Image} from 'expo-image'
