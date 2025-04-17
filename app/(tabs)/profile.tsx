d8c41e25e982a43ad30f781cf0770c91cbbc52c9
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-04-17 17:47:28 +0200

    wrote code for register

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,13 +1,44 @@
 import {View, Text} from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useState, useEffect } from 'react'
+import Register from '@/components/register'
 
-export default function profile(){
+interface User {
+    name: string
+     number: string
+     address: string
+}
 
-    return(<View
-        style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-          }}>
-    <Text>This will Show the user profile</Text>
-    </View>)
+export default function Profile() {
+    const [user, setUser] = useState<User[]>([])
+
+    useEffect(() => {
+        const loadUsers = async () => {
+            try {
+                const jsonData = await AsyncStorage.getItem('user_data')
+                setUser(jsonData != null ? JSON.parse(jsonData) : [])
+            } catch (error) {
+                alert(error)
+            }
+        }
+        loadUsers()
+    }, [])
+
+    if(user != null){
+        return (
+            <View>
+                {user.map((user, index) => (
+                    <View key={index}>
+                        <Text>{user.name}</Text>
+                        <Text>{user.number}</Text>
+                        <Text>{user.address}</Text>
+                    </View>
+                ))}
+            </View>
+        )
+    }else{
+      return(
+        <Register />
+      )
+    }
 }
