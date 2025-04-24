2752f410d5216e40f66919808b76d19e73ff66ba
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-04-24 09:56:26 +0200

    added a pop up to display the length of the cart

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,17 +1,27 @@
 import { Tabs } from "expo-router";
 import {Ionicons} from '@expo/vector-icons'
+import { useCart } from "@/logic/useCart";
 
 export default function TabsLayout() {
+
+  const { cart } = useCart()
+  const cartCount = cart.length
+
   return(
     <Tabs>
       <Tabs.Screen name="index" 
       options={{
-        title: "Our Menu",
-        headerTitleAlign: "center",
+        title: "Darys \t\t\t\t\t\t\t\t\t\t Our Menu",
+        headerTitleAlign: "left",
+        headerTitleContainerStyle: {
+          paddingLeft: 20,
+        },
         headerTintColor: "white",
         headerStyle: {
           backgroundColor: "orange",
         },
+       
+       
         tabBarLabel: "Meals",
         tabBarIcon: ({ color }) => (
           <Ionicons name="fast-food" size={24} color={color} />
@@ -23,7 +33,7 @@ export default function TabsLayout() {
 
       <Tabs.Screen name="order"
       options={{
-        title: "My Orders",
+        title: "Your Orders",
         headerTitleAlign: "center",
         headerTintColor: "white",
         headerStyle: {
@@ -33,6 +43,16 @@ export default function TabsLayout() {
         tabBarIcon: ({color}) => (
           <Ionicons name="cart" size={24} color={color} />
         ),
+        tabBarBadge: cartCount > 0 ? cartCount : undefined,
+        tabBarBadgeStyle: {
+          backgroundColor: "orange",
+          color: "white",
+          fontWeight: "bold",
+          fontSize: 12,
+          top: -5,
+          padding: 2,
+          borderRadius: 10,}
+        ,
         tabBarActiveTintColor: "orange",
         tabBarInactiveTintColor: "gray",
       }} />
