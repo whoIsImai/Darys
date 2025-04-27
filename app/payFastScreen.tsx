6557fafdd0beadd08a415b88e9d42b71e6cfffbc
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-04-27 19:10:52 +0200

    add to order store after each successful payment

--- app/payFastScreen.tsx
+++ app/payFastScreen.tsx
@@ -1,13 +1,17 @@
 import { Alert } from 'react-native';
 import { WebView } from 'react-native-webview'
-import { View, Text } from 'react-native';
-import { useRoute } from '@react-navigation/native';
+import { View, Text } from 'react-native'
+import { useRoute } from '@react-navigation/native'
+import { useOrderStore } from '@/logic/orderStore'
+import { useCart } from '@/logic/useCart'
 
 interface RouteParams {
   payfastURL: string;
 }
 
 export default function PayfastScreen() {
+  const { setLatestOrder } = useOrderStore()
+  const {cart, total} = useCart()
   const route = useRoute()
   const { payfastURL } = route.params as RouteParams
 
@@ -19,6 +23,12 @@ export default function PayfastScreen() {
     )
   }
 
+  const order = {
+    name: cart.map(item => `${item.name} X ${item.quantity}`).join(', '),
+    img: cart.map(item => item.img).join(', '),
+    price: total,
+    date: new Date().toLocaleDateString()
+  }
 
   return (
     <View style={{ flex: 1 }}>
@@ -27,6 +37,8 @@ export default function PayfastScreen() {
       onNavigationStateChange={(navState) => {
         if (navState.url.includes('https://payment-messages.vercel.app/success')) {
           Alert.alert('Payment Successful', 'Your payment was successful!')
+          setLatestOrder(order)
+          
         } else if (navState.url.includes('https://payment-messages.vercel.app/failed')) {
             Alert.alert('Payment Failed', 'Your payment failed. Please try again.')
         }
