5cf612cc833393b10c6f1de212fa567239b05290
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-04-27 19:12:21 +0200

    removed unnecessary import

--- logic/orderStore.tsx
+++ logic/orderStore.tsx
@@ -1,6 +1,5 @@
 import { create } from 'zustand'
 import AsyncStorage from '@react-native-async-storage/async-storage'
-import { persist, createJSONStorage } from 'zustand/middleware'
 
 type Order ={
     name: string
