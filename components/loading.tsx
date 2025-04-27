841c1d4bae7a4b5ce5cf5b5a1e628f309f280ca7
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-04-27 19:00:28 +0200

    made the loader to be overlay

--- components/loading.tsx
+++ components/loading.tsx
@@ -2,10 +2,15 @@ import { View, StyleSheet, ActivityIndicator } from "react-native"
 
 const styles = StyleSheet.create({
     loaderContainer: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#f5fcff',
+        position: 'absolute', 
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        zIndex: 1000,
+        justifyContent: 'center',
+        alignItems: 'center',
+       backgroundColor: 'rgba(255, 255, 255, 0.8)'
     },
   })
 
