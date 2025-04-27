92c0b2b094fe175c0e40cd697918d70b1a8b0eb5
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-04-27 17:21:55 +0200

    created view for the spinner

---
+++ components/loading.tsx
@@ -0,0 +1,14 @@
+import { View, StyleSheet } from "react-native"
+
+const styles = StyleSheet.create({
+    loader: {
+
+    }
+})
+
+export default function Loading() {
+    return (
+        <View style={styles.loader}>
+        </View>
+    )
+}
