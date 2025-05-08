0a9c5a3ef7b8f25b839e61d2d8ef17c407af4175
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-05-08 20:38:50 +0200

    reverted back

--- components/KotaMenu.tsx
+++ components/KotaMenu.tsx
@@ -10,7 +10,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
        padding: 10,
-        backgroundColor: "#fff",
+        backgroundColor: '#fff',    
         position: 'relative',
         top: -40,
     },
